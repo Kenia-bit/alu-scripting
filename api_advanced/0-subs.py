0c1bd1d43ef2d65d820ff51fc75ed545e47d34be
Kenia-bit <u.kenia@alustudent.com>
2025-10-26 19:46:03 +0200

    subs

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,22 +1,15 @@
 #!/usr/bin/python3
 """Return number of subscribers for a given subreddit"""
-
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the number of subscribers for a given subreddit."""
-    if not subreddit or not isinstance(subreddit, str):
-        return 0
-
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "MyRedditApp/1.0"}
-
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code != 200:
-            return 0
-        data = response.json().get("data", {})
-        return data.get("subscribers", 0)
-    except Exception:
-        return 0
+    """Return the number of subscribers """
+    url = "https://www.reddit.com/r/{}/about.json" \
+        .format(subreddit)
+    headers = {'User-Agent': 'My User Agent 1.0'}
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        return response.json().get('data') \
+            .get('subscribers')
+    return 0
