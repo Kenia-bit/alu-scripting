6ec7f0c3dacd9a6c687ee6d2afdfd62b7edd2767
Kenia-bit <u.kenia@alustudent.com>
2025-10-26 19:42:10 +0200

    Top-ten

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,27 +1,22 @@
 #!/usr/bin/python3
-"""Module 1-top_ten
-Print the titles of the first 10 hot posts of a subreddit."""
+"""Print the titles of the first 10Hot Posts"""
 import requests
 
 
 def top_ten(subreddit):
-    """Print the titles of the first 10 hot posts of a subreddit."""
-    headers = {'User-Agent': 'alu-scripting/1.0'}
-    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    """The top ten titles"""
+    headers = {'User-Agent': 'MyAPI/0.0.1'}
+    url = "https://reddit.com/r/{}.json".format(subreddit)
+    response = requests.get(url, headers=headers)
 
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code != 200:
-            return  # nothing printed for invalid subreddit
-
-        data = response.json().get('data', {})
-        children = data.get('children', [])
-
-        # Print exactly up to 10 titles
-        for i in range(min(10, len(children))):
-            title = children[i].get('data', {}).get('title')
-            if title:
-                print(title)
-
-    except Exception:
-        return
+    if response.status_code == 200:
+        json_data = response.json()
+        for i in range(10):
+            print(
+                    json_data.get('data')
+                    .get('children')[i]
+                    .get('data')
+                    .get('title')
+                )
+    else:
+        print(None)
