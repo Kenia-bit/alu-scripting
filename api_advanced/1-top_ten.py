f675d676a9d0d2af5f9bd6bbbc13e7eb05e2de63
Kenia-bit <u.kenia@alustudent.com>
2025-10-26 13:59:30 +0200

    Top-ten

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python3
-"""Print the titles of the first 10Hot Posts"""
+"""Print the titles of the first 10 hot posts"""
 import requests
 
 
 def top_ten(subreddit):
     """The top ten titles"""
     headers = {'User-Agent': 'MyAPI/0.0.1'}
-    url = "https://reddit.com/r/{}.json".format(subreddit)
-    response = requests.get(url, headers=headers)
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
 
-    if response.status_code == 200:
-        json_data = response.json()
-        for i in range(10):
-            print(
-                    json_data.get('data')
-                    .get('children')[i]
-                    .get('data')
-                    .get('title')
-                )
-    else:
-        print(None)
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code != 200:
+            return  # silently return for invalid subreddit
+
+        children = response.json().get('data', {}).get('children', [])
+        for post in children:
+            title = post.get('data', {}).get('title')
+            if title:
+                print(title)
+
+    except Exception:
+        return  # silently ignore errors
